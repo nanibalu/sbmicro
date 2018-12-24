e77be5eaaf0be218da2cfdf68962c1c7e7f9d6db
nani1 <nani1@192.168.100.6>
2018-12-24 09:10:36 +0530

    My Changes

---
+++ springbootswagger/src/main/java/com/verizon/sb/swagg/SpringbootswaggerApplication.java
@@ -0,0 +1,14 @@
+package com.verizon.sb.swagg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootswaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootswaggerApplication.class, args);
+	}
+
+}
+
